0acc0e32f403e38ea989d8bb3caa610cb8192584
Ivan <support@wintuning.ru>
2020-11-19 19:11:26 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Импортируем сам React
+import React from 'react'
+//Импортируем метод render из библиотеки react-dom
+import {render} from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function Article() {
+    const body = <section>body</section>
+    return (
+        <div>
+            <h2>title</h2>
+            {body}
+            <h3>creation date: {(new Date()).toDateString()}</h3>
+        </div>
+    )
+}
+
+
+function App() {
+    return (
+        <div>
+            <h1>App name</h1>
+            <Article/>
+        </div>
+    )
+}
+
+
+render(<App/>, document.getElementById('root'))
